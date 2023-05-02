7a6a0e360c06ad5a8800663b78d9134bc46238f4
hammadmajeed <hammadmajeed@gmail.com>
2023-05-02 09:57:27 +0500

    updates

--- train.py
+++ train.py
@@ -27,7 +27,6 @@ def eval_metrics(actual, pred):
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(40)
-
     # Read the wine-quality csv file (make sure you're running this from the root of MLflow!)
     wine_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "wine-quality.csv")
     data = pd.read_csv(wine_path)
